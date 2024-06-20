7a13c26746e06a5694c976ec44c685d998715762
cslucki <34413632+cslucki@users.noreply.github.com>
2024-06-20 12:38:53 +0200

    Commit du 2024-06-20 12:38:53

--- inc/theme/layouts/standard.php
+++ inc/theme/layouts/standard.php
@@ -1,20 +1,28 @@
 <?php 
 /**************************************/
 ## Standard blog layout
+# modifié le 25/06/2019 à 12h30
 /**************************************/
 
 ?>
 
 <div class="standard-container">
 	<?php 
-		
-		if(have_posts()):
-			while(have_posts()): the_post();
+		$args = array(
+			'orderby' => 'date',
+			'order'   => 'ASC'
+		);
+
+		$query = new WP_Query($args);
+
+		if($query->have_posts()):
+			while($query->have_posts()): $query->the_post();
 				
 				get_template_part('inc/theme/layouts/standard/content'); 
 				
 			endwhile;
 			
+			wp_reset_postdata();
 		else:
 			get_template_part('inc/theme/views/content-none'); 
 		endif; 
